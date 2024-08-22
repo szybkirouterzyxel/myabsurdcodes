453ce50a00b86a21046503500d227ed333b6ed90
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2024-08-23 01:44:46 +0200

    Comit

---
+++ UserInteraction/UserInteraction/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("wpisz cos: ");
+string interakcja = Console.ReadLine();
+Console.WriteLine("litera/liczba to: " + interakcja);
