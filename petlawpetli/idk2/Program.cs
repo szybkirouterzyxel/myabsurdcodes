7e0c56f117fb26bf97c3ce3258fb6255878f5b15
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2024-08-23 01:33:11 +0200

    Csharp

---
+++ petlawpetli/idk2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int counter = 0;
+while (counter < 10)
+{
+    int k = 0;
+    while (k < 10)
+    {
+        Console.WriteLine("kot");
+        counter++;
+    }
+    
+
+    counter++;
+}
+
