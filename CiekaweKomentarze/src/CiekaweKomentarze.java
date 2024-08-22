44e6558ebf678d3bcdfcaeac165f7b4b9a563ea9
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2024-08-23 00:08:59 +0200

    Add files via upload

---
+++ CiekaweKomentarze/src/CiekaweKomentarze.java
@@ -0,0 +1,13 @@
+public class Main {
+    public void main() throws InterruptedException {
+        System.out.println("beatbox");
+        Thread.sleep(Long.parseLong("1000"));
+        System.out.println("why so serious 🤔🤔🥵🥵🙏🔥😈"); // so sigma 🗿
+        Thread.sleep(1000);
+        System.out.println("beatbox"); // 14 linijek kodu 🥶🥶
+        Thread.sleep(1000);
+        System.out.println("beatbox"); // MokrySpierd
+
+
+    }
+}
